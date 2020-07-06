ff11ae4342c21a3ebe29eeb9c5570a17609c1ef6
lilia <zeinab_33@mail.ru>
2020-07-06 19:55:46 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListContactsComponent } from "./list-contacts/list-contacts.component";
+import { AddContactComponent } from "./add-contact/add-contact.component";
+import { EditContactComponent } from "./edit-contact/edit-contact.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "list-contacts", component: ListContactsComponent },
+  { path: "add-contact", component: AddContactComponent },
+  { path: "edit-contact", component: EditContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ListContactsComponent } from './list-contacts/list-contacts.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent,
+    ListContactsComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
