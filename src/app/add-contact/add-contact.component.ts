96a475581ee116ad3c1552b601f9b39e620140f8
lilia <zeinab_33@mail.ru>
2020-07-08 10:57:52 +0300

    add contact form

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-contact',
-  templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.scss']
+  selector: "app-add-contact",
+  templateUrl: "./add-contact.component.html",
+  styleUrls: ["./add-contact.component.scss"],
 })
 export class AddContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AddContactComponent } from './add-contact/add-contact.component';
-import { ListContactsComponent } from './list-contacts/list-contacts.component';
-import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AddContactComponent } from "./add-contact/add-contact.component";
+import { ListContactsComponent } from "./list-contacts/list-contacts.component";
+import { EditContactComponent } from "./edit-contact/edit-contact.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AddContactComponent,
     ListContactsComponent,
-    EditContactComponent
+    EditContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
