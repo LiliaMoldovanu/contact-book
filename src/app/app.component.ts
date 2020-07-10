ccba66ef4c2a17a1506fe5a1facb4d9d38bf0c83
lilia <zeinab_33@mail.ru>
2020-07-10 20:05:26 +0300

    outputting contacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
-import { Contact } from "./contact.model";
 
 @Component({
   selector: "app-root",
@@ -10,29 +7,6 @@ import { Contact } from "./contact.model";
 })
 export class AppComponent {
   title = "contact-book";
-  loadedContacts: Contact[] = [];
 
-  constructor(private http: HttpClient) {}
-
-  onFetchContacts() {
-    this.http
-      .get<{ [key: string]: Contact }>(
-        "https://contact-book-60025.firebaseio.com/contacts.json"
-      )
-      .pipe(
-        map((responseData) => {
-          const contactsArray: Contact[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              contactsArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return contactsArray;
-        })
-      )
-      .subscribe((contacts) => {
-        this.loadedContacts = contacts;
-        console.log(this.loadedContacts);
-      });
-  }
+  constructor() {}
 }
--- src/app/list-contacts/list-contacts.component.ts
+++ src/app/list-contacts/list-contacts.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { map } from "rxjs/operators";
+import { Contact } from "../contact.model";
 
 @Component({
-  selector: 'app-list-contacts',
-  templateUrl: './list-contacts.component.html',
-  styleUrls: ['./list-contacts.component.scss']
+  selector: "app-list-contacts",
+  templateUrl: "./list-contacts.component.html",
+  styleUrls: ["./list-contacts.component.scss"],
 })
 export class ListContactsComponent implements OnInit {
+  loadedContacts: Contact[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.onFetchContacts();
   }
 
+  onFetchContacts() {
+    this.http
+      .get<{ [key: string]: Contact }>(
+        "https://contact-book-60025.firebaseio.com/contacts.json"
+      )
+      .pipe(
+        map((responseData) => {
+          const contactsArray: Contact[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              contactsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return contactsArray;
+        })
+      )
+      .subscribe((contacts) => {
+        this.loadedContacts = contacts;
+        console.log(this.loadedContacts);
+      });
+  }
 }
